37a90bc813c968a9faea99ba2ad91ebe16761dd1
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-09-09 13:20:40 +0530

    Added correct QR, Clickable, Changed theme

--- QR Gen.py
+++ QR Gen.py
@@ -3,7 +3,7 @@ import time
 from PIL import Image
 
 
-link = "https://example.com"
+link = "http://127.0.0.1:3000/index1.html"
 
 # Function to generate and display a QR code
 def generate_qr_code(link):
@@ -19,12 +19,12 @@ def generate_qr_code(link):
     img = qr.make_image(fill_color="black", back_color="white")
 
     # Display the QR code image
-    img.save("QR.png")
+    img.save("qrcode.png")
 
 # Generate and display QR codes every 5 seconds
 try:
     while True:
         generate_qr_code(link)
-        time.sleep(5)
+        time.sleep(1)
 except KeyboardInterrupt:
     print("QR code generation stopped.")
