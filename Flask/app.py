c0f14bf38f8208031d66306a8357cab64f4cb9a4
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-09-09 19:20:51 +0530

    Made the Submit button to make POST request in Teacher Panel

--- Flask/app.py
+++ Flask/app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 # Simulated database of students (you can replace this with a real database)
 students = [
-    {"id": 1, "name": "Student 1"},
-    {"id": 2, "name": "Student 2"},
-    {"id": 3, "name": "Student 3"},
+    {"id": 1, "name": "Mohd Azeem"},
+    {"id": 2, "name": "Dheeraj Jha"},
+    {"id": 3, "name": "Shantanu Pant"},
+    {"id": 4, "name": "Student 1"},
+    {"id": 5, "name": "Student 2"},
+    {"id": 6, "name": "Student 3"},
 ]
 
 # Route to render the main HTML template
@@ -21,10 +24,23 @@ def index():
 
     return render_template("index.html", students=students)
 
-# Route to render the "Add Manually" page
-@app.route("/add_manually")
+# Route to render the "Add Manually" page (GET request)
+@app.route("/add_manually", methods=["GET"])
 def add_manually():
     return render_template("index1.html")
+
+# Route to handle form submissions (POST request)
+@app.route("/add_manually_post", methods=["POST"])
+def add_manually_post():
+    # Get the selected student from the form
+    selected_student = request.form.get("student")
+
+    # Add the selected student to the list of students
+    if selected_student:
+        students.append({"id": len(students) + 1, "name": selected_student})
+
+    return redirect("/add_manually")  # Redirect back to the "Add Manually" page after submission
+
 
 if __name__ == "__main__":
     app.run(debug=True)
