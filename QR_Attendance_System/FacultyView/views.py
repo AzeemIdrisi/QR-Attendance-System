1b62fe160b9f61dd329e2b37f4972565b7dccc3b
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-11-10 13:33:35 +0530

    Added QRCode Logic for Django

--- QR_Attendance_System/FacultyView/views.py
+++ QR_Attendance_System/FacultyView/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from .models import Student
+import qrcode
+import socket
 
-# Create your views here.
+
+def qrgenerator():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+
+    link = f"http://{ip}:8000/add_manually"
+
+    # Function to generate and display a QR code
+    def generate_qr_code(link):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(link)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("FacultyView/static/FacultyView/qrcode.png")
+
+    generate_qr_code(link)
 
 
 def faculty_view(request):
+    qrgenerator()
     students = Student.objects.all().order_by("s_roll")
     return render(
         request,
