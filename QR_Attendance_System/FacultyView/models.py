608d778915c0103d3fb249be087e74a543c81157
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-11-10 13:21:30 +0530

    Added Data to Database

--- QR_Attendance_System/FacultyView/models.py
+++ QR_Attendance_System/FacultyView/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+)
+
 
 # Create your models here.
+class Student(models.Model):
+    s_roll = models.CharField(max_length=20, primary_key=True)
+    s_fname = models.CharField(max_length=20)
+    s_lname = models.CharField(max_length=20)
+    s_branch = models.CharField(max_length=10)
+    s_section = models.CharField(max_length=5)
+    s_year = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)],
+    )
+
+    def __str__(self) -> str:
+        return f"{self.s_fname} {self.s_lname} - {self.s_roll} - {self.s_branch}({self.s_year}{self.s_section})"
--- QR_Attendance_System/FacultyView/views.py
+++ QR_Attendance_System/FacultyView/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
-students = [
-    {"id": 1, "name": "Mohd Azeem"},
-    {"id": 2, "name": "Dheeraj Jha"},
-    {"id": 3, "name": "Shantanu Pant"},
-    {"id": 4, "name": "Student 1"},
-    {"id": 5, "name": "Student 2"},
-    {"id": 6, "name": "Student 3"},
-]
-
 
 def faculty_view(request):
+    students = Student.objects.all().order_by("s_roll")
     return render(
         request,
         "FacultyView/FacultyViewIndex.html",
