ff01d4b0a5e82cbe3fe35dc167691dd0ccb2f526
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-11-12 13:43:09 +0530

    Added Relational Models

--- FacultyView/admin.py
+++ FacultyView/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Student
+from .models import Student, Section, Year, Branch
 
 admin.site.register(Student)
+admin.site.register(Section)
+admin.site.register(Year)
+admin.site.register(Branch)
--- FacultyView/models.py
+++ FacultyView/models.py
@@ -5,16 +5,36 @@ from django.core.validators import (
 )
 
 
-# Create your models here.
+class Section(models.Model):
+    section = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return self.section
+
+
+class Year(models.Model):
+    year = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)],
+    )
+
+    def __str__(self) -> str:
+        return str(self.year)
+
+
+class Branch(models.Model):
+    branch = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.branch
+
+
 class Student(models.Model):
     s_roll = models.CharField(max_length=20, primary_key=True)
     s_fname = models.CharField(max_length=20)
     s_lname = models.CharField(max_length=20)
-    s_branch = models.CharField(max_length=10)
-    s_section = models.CharField(max_length=5)
-    s_year = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(4)],
-    )
+    s_branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    s_section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    s_year = models.ForeignKey(Year, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.s_fname} {self.s_lname} - {self.s_roll} - {self.s_branch}({self.s_year}{self.s_section})"
